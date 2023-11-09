961a0582be2d0ec1dc0b2a2a9ae807c8a5aeffe6
Brian <briantolentino311@gmail.com>
2023-11-09 16:55:47 +0800

    feat/create-table-animals

--- veterinary.sql
+++ veterinary.sql
@@ -10,3 +10,17 @@ phone VARCHAR(100),
 email VARCHAR(100),
 PRIMARY KEY (ownerid)
 );
+
+CREATE TABLE animals (
+
+animalid INT PRIMARY KEY,
+name VARCHAR(50),
+species VARCHAR(50),
+breed VARCHAR(50),
+dateofbirth DATE,
+gender VARCHAR(10),
+color VARCHAR(50),
+ownerid INT,
+FOREIGN KEY ownerid REFERENCES owners(ownerid)
+
+);
