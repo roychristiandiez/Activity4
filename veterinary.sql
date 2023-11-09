02ea5c26bc44c5efc374c713b840910e8c0f07a9
roychristiandiez <diezc68@gmail.com>
2023-11-09 16:49:56 +0800

    feat/create-table-owners

--- veterinary.sql
+++ veterinary.sql
@@ -1,1 +1,12 @@
 CREATE DATABASE veterinary;
+
+CREATE TABLE owners (
+
+ownerid INT,
+ofirstname VARCHAR(50),
+olastname VARCHAR(50),
+address VARCHAR(100),
+phone VARCHAR(100),
+email VARCHAR(100),
+PRIMARY KEY (ownerid)
+);
